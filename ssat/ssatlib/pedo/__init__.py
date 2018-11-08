abdffc850d2454a554b1bed0477438e04987a3e5
finkler <thomas.kreuzer@rwth-aachen.de>
2018-11-08 08:44:09 +0100

    New

--- ssat/ssatlib/pedo/__init__.py
+++ ssat/ssatlib/pedo/__init__.py
@@ -1,5 +1,12 @@
 import numpy as np
 
+from ssat.ssatlib.stats import loglap
+
+
+def d(i):
+    base = 0.002 if i % 2 == 0 else 0.0063
+    return base * 10**i
+
 
 def phi(d):
     return -np.log2(d)
