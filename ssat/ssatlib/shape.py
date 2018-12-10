dde99543f04a0d7abd13bc67a63ea4a8b2d87712
finkler <thomas.kreuzer@rwth-aachen.de>
2018-12-10 09:31:19 +0100

    Xena

--- ssat/ssatlib/shape.py
+++ ssat/ssatlib/shape.py
@@ -9,37 +9,49 @@ class Edge:
 
 
 class Polygon:
-    def __init__(self, points=[]):
-        self.points = points
+    def __init__(self, points):
+        self.__points = points
+        self.__len = len(points)
 
     def __iter__(self):
         self.curr = 0
         return self
 
     def __len__(self):
-        return len(self.points) + 1
+        return self.__len
 
     def __next__(self):
-        n = len(self.points)
         i = self.curr
-        if i > n or n == 0:
+        if i > self.__len or self.__len == 0:
             raise StopIteration
-        if i == n:
+        if i == self.__len:
             i = 0
         self.curr += 1
-        return self.points[i]
+        return self.__points[i]
 
     def bounds(self):
-        pass
+        xmin = self.__points[0][0]
+        ymin = self.__points[0][1]
+        xmax = self.__points[0][0]
+        ymax = self.__points[0][1]
+        for x, y in self.__points:
+            if xmin > x:
+                xmin = x
+            if xmax < x:
+                xmax = x
+            if ymin > y:
+                ymin = y
+            if ymax < y:
+                ymax = y
+        return (xmin, ymin, xmax, ymax)
 
     def edges(self):
-        n = len(self.points)
         e = []
-        for i in range(n):
+        for i in range(self.__len):
             j = i + 1
-            if j == n:
+            if j == self.__len:
                 j = 0
-            e.append(Edge(self.points[i], self.points[j], False))
+            e.append(Edge(self.__points[i], self.__points[j], False))
         return e
 
     def contains(self, p):
