588e78e3366896ffb9f69ff64c74adc6a4e9ceb4
Thomas Kreuzer <thomas.kreuzer@rwth-aachen.de>
2019-10-09 14:01:15 +0200

    The Titans

--- src/stabpack.f90
+++ src/stabpack.f90
@@ -4,6 +4,57 @@ module razdol
   public razslv
 
 contains
+  pure function razdrv(n, w, c, phi, u, alpha, b, h, f, e, t) result(mu)
+    integer, intent(in) :: n
+    real, dimension(n), intent(in) :: w, c, phi, u, alpha, b
+    real, intent(in) :: h(n + 1), f
+    real, dimension(n + 1), intent(in) :: e, t
+    integer :: i, j
+    real, dimension(n + 1) :: de, dt
+    real :: cosa2, mu, tana, tanp
+
+    de = 0
+    dt = 0
+    do i = 2, n + 1
+      j = i - 1
+      cosa2 = cos(alpha(j))**2
+      tana = tan(alpha(j))
+      tanp = tan(phi(j))
+      de(i) = cosa2 * (6. * f * dt(j) * (f * tana - tanp) &
+            + (2. * b(j) * w(j) * h(j) + b(j) * w(j) * h(i) &
+            + 6. * t(j) - 6. * e(j) * tana) * tanp + 3. * b(j)/cosa2 &
+            * (c(j) - u(j) * tanp) + 3. * f * de(j) &
+            * (f - f * tana**2 + 2. * tana * tanp))
+      de(i) = de(i) / (3. * f**2)
+      dt(i) = (de(i) + de(j)) * tana + dt(j)
+    end do
+    i = maxloc(e, 1)
+    mu = (de(i) * e(n + 1) - de(n + 1) * e(i)) / e(i)**2
+  end function
+
+  pure subroutine razd(f, wa, fval, deriv)
+    integer, parameter :: nwa = 7
+    real, intent(in) :: f, wa(:)
+    real, intent(out) :: fval, fderiv
+    real, dimension(size(wa)/nwa+1) :: e, t
+    real :: mu
+    integer :: n
+
+    n = size(wa) / nwa
+    associate(w => wa(:n),
+      c => wa(n+1:2*n),
+      phi => wa(2*n+1:3*n),
+      u => wa(3*n+1:4*n),
+      alpha => wa(4*n+1:5*n),
+      b => wa(5*n+1:6*n),
+      h => wa(6*n+1:))
+
+      call razslv(n, w, c, phi, u, alpha, b, h, f, e, t, mu)
+      fval = mu - 1.
+      fderiv = razdrv(n, w, c, phi, u, alpha, b, h, f, e, t)
+    end associate
+  end subroutine
+
   pure subroutine razslv(n, w, c, phi, u, alpha, b, h, f, e, t, mu, p0)
     integer, intent(in) :: n
     real, dimension(n), intent(in) :: w, c, phi, u, alpha, b
@@ -34,32 +85,4 @@ contains
     end do
     mu = 1. - e(n + 1) / maxval(e)
   end subroutine
-
-  pure function razsld(n, w, c, phi, u, alpha, b, h, f, e, t) result(mu)
-    integer, intent(in) :: n
-    real, dimension(n), intent(in) :: w, c, phi, u, alpha, b
-    real, intent(in) :: h(n + 1), f
-    real, dimension(n + 1), intent(in) :: e, t
-    integer :: i, j
-    real, dimension(n + 1) :: de, dt
-    real :: cosa2, mu, tana, tanp
-
-    de = 0
-    dt = 0
-    do i = 2, n + 1
-      j = i - 1
-      cosa2 = cos(alpha(j))**2
-      tana = tan(alpha(j))
-      tanp = tan(phi(j))
-      de(i) = cosa2 * (6. * f * dt(j) * (f * tana - tanp) &
-            + (2. * b(j) * w(j) * h(j) + b(j) * w(j) * h(i) &
-            + 6. * t(j) - 6. * e(j) * tana) * tanp + 3. * b(j)/cosa2 &
-            * (c(j) - u(j) * tanp) + 3. * f * de(j) &
-            * (f - f * tana**2 + 2. * tana * tanp))
-      de(i) = de(i) / (3. * f**2)
-      dt(i) = (de(i) + de(j)) * tana + dt(j)
-    end do
-    i = maxloc(e, 1)
-    mu = (de(i) * e(n + 1) - de(n + 1) * e(i)) / e(i)**2
-  end function
 end module
