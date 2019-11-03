115e364f79384d781c93ade0bde623b3633e68f9
finkler <thomas.kreuzer@rwth-aachen.de>
2019-11-03 11:10:54 +0000

    Usage

--- src/seep.f90
+++ src/seep.f90
@@ -6,9 +6,9 @@ program main
 
   character(len=255) :: arg, datafile, precfile, useage
   integer :: bins, i, id
-  real :: dt, xlim(2)
+  real :: dt, dx, dy, xlim(2)
 
-  usage = 'usage: seep [-bnum] [-tstep] -Pprecfile [-x0|1value] file'
+  usage = 'usage: seep [-bnum] [-dx|yvalue] [-tstep] [-Pprecfile] [-x0|1value] file'
   bins = 100
   dt = 1.
   xlim = 0
@@ -18,6 +18,14 @@ program main
       select case(arg(2:2))
       case('b')
         read(arg(3:), *) bins
+      case('d')
+        if(arg(3:3) == 'x') then
+          read(arg(4:), *) dx
+        else if(arg(3:3) == 'y') then
+          read(arg(4:), *) dy
+        else
+          call fatal(usage)
+        end if
       case('t')
         read(arg(3:), *) dt
       case('P')
