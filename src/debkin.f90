2498c77a4c61b2b0d4beb2ac812117f053541b8b
Thomas Kreuzer <thomas.kreuzer@rwth-aachen.de>
2019-11-26 19:29:13 +0100

    Girls Just Wanna Have Fun

--- src/debkin.f90
+++ src/debkin.f90
@@ -67,12 +67,11 @@ contains
       h = y1(j) - y1(i)
       s = sqrt((x(j) - x(i))**2 + h**2)
       alpha = asin(h / s)
-      drag = h / (cos(alpha) * s * log(s))
+      drag = h / (2. * cos(alpha) * s)
       write(msg, '(A5,1X,F9.4)') 'DRAG:', drag
       call alert(msg)
     end if
-    k = kin_(x, y1, drag, 1)
-    k = k + kin_(x, y1, drag, -1)
+    k = kin_(x, y1, drag, 1) + kin_(x, y1, drag, -1)
     k = k / maxval(k)
     if(k(i) /= 1) call fatal('invalid drag.')
     y2 = y1 - k * (depth(1) - depth(2)) + depth(2)
@@ -92,7 +91,7 @@ contains
     do i = e1, e2, e3
       j = i - 1
       h = dir * (y(j) - y(i))
-      s = sqrt((x(i) + x(j))**2 + h**2)
+      s = sqrt((x(i) - x(j))**2 + h**2)
       alpha = asin(h / s)
       ii = i
       jj = j
